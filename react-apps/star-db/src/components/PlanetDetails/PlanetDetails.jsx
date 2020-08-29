a0b7637234975d9908563e4c8058f7747733415b
Alexander Mandrov <mandrov.ad17@physics.msu.ru>
2020-08-30 00:39:32 +0300

    basic project structure and transforming api data

---
+++ react-apps/star-db/src/components/PlanetDetails/PlanetDetails.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './PlanetDetails.scss';
+
+const PlanetDetails = () => {
+  return (
+  
+  );
+};
+
+export default PlanetDetails;
---
+++ react-apps/star-db/src/components/StarshipDetails/StarshipDetails.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './StarshipDetails.scss';
+
+const StarshipDetails = () => {
+  return (
+  
+  );
+};
+
+export default StarshipDetails;
