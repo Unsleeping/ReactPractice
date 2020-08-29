cff4078aa9cd35f8eca66422919eaa9b44686212
Alexander Mandrov <mandrov.ad17@physics.msu.ru>
2020-08-30 02:19:29 +0300

    add PlanetView component

--- react-apps/star-db/src/components/RandomPlanet/RandomPlanet.jsx
+++ react-apps/star-db/src/components/RandomPlanet/RandomPlanet.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import SwapiService from '../../services/swapi-service';
 import { css } from "@emotion/core";
 import CircleLoader from "react-spinners/CircleLoader";
+import PlanetView from './PlanetView';
 import './RandomPlanet.scss';
 
 // Can be a string as well. Need to ensure each key-value pair ends with ;
@@ -26,45 +27,28 @@ export default class RandomPlanet extends Component {
   }
 
   onPlanetLoaded = (planet) => {
-    this.setState({planet});
+    this.setState({
+      planet,
+      loading: false
+    });
   };
 
   updatePlanet() {
-    const id = Math.floor(Math.random() * 20) + 1;
+    const id = Math.floor(Math.random() * 18) + 2;
     this.swapiService.getPlanet(id).then(this.onPlanetLoaded);
   }
 
   render() {
 
-    const { planet: { id, name, population, rotationPeriod, diameter } } = this.state;
+    const { loading } = this.state;
+
+    const spinner = loading ? <CircleLoader css={override} size={150} color={"teal"} loading={this.state.loading}/> : null;
+    const content = !loading ? <PlanetView planet={this.state.planet} /> : null;
 
     return (
       <div className="random-planet jumbotron rounded">
-        <CircleLoader
-          css={override}
-          size={200}
-          color={"teal"}
-          loading={this.state.loading}
-        />
-        <img className="planet-image"
-              src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
-        <div>
-          <h4>{name}</h4>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <span className="term">Population</span>
-              <span>{population}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Rotation Period</span>
-              <span>{rotationPeriod}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Diameter</span>
-              <span>{diameter}</span>
-            </li>
-          </ul>
-        </div>
+        {spinner}
+        {content}
       </div>
     );
   }
