ffbdbbc7da4b1bf2281d8d05dfd95f967c27b53a
Alexander Mandrov <mandrov.ad17@physics.msu.ru>
2020-08-30 01:14:47 +0300

    try to eject

--- react-apps/star-db/src/components/RandomPlanet/RandomPlanet.jsx
+++ react-apps/star-db/src/components/RandomPlanet/RandomPlanet.jsx
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import SwapiService from '../../services/swapi-service';
+import { css } from "@emotion/core";
+import ClipLoader from "react-spinners/ClipLoader";
 import './RandomPlanet.scss';
+
+// Can be a string as well. Need to ensure each key-value pair ends with ;
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+`;
 
 export default class RandomPlanet extends Component {
 
   swapiService = new SwapiService();
 
   state = {
-    id: null,
-    name: null,
-    population: null,
-    rotationPeriod: null,
-    diameter: null
+    planet: {}
   };
 
   constructor() {
@@ -19,25 +24,28 @@ export default class RandomPlanet extends Component {
     this.updatePlanet();
   }
 
+  onPlanetLoaded = (planet) => {
+    this.setState({planet});
+  };
+
   updatePlanet() {
     const id = Math.floor(Math.random() * 20) + 1;
-    this.swapiService.getPlanet(id).then((planet) => {
-      this.setState({
-        id,
-        name: planet.name,
-        population: planet.population,
-        rotationPeriod: planet.rotation_period,
-        diameter: planet.diameter
-      });
-    });
+    this.swapiService.getPlanet(id).then(this.onPlanetLoaded);
   }
 
   render() {
 
-    const { id, name, population, rotationPeriod, diameter } = this.state;
+    const { planet: { id, name, population, rotationPeriod, diameter } } = this.state;
 
     return (
       <div className="random-planet jumbotron rounded">
+        <Loader
+            type="Puff"
+            color="#00BFFF"
+            height={100}
+            width={100}
+            timeout={3000} 
+        />
         <img className="planet-image"
               src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
         <div>
