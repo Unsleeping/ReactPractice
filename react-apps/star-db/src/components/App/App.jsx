9f8e2d7120589680bbfdd60b0cc000b5abfd338f
Alexander Mandrov <mandrov.ad17@physics.msu.ru>
2020-09-10 17:11:12 +0300

    context, dependency injection

--- react-apps/star-db/src/components/App/App.jsx
+++ react-apps/star-db/src/components/App/App.jsx
@@ -4,13 +4,9 @@ import './App.scss';
 import Header from '../Header';
 import RandomPlanet from '../RandomPlanet';
 import ErrorIndicator from '../ErrorIndicator';
-import PeoplePage from '../PeoplePage/PeoplePage';
-
-import ItemList from '../ItemList';
-import Record from '../Record';
 
 import SwapiService from '../../services/swapi-service';
-import ItemDetails from '../ItemDetails';
+import { SwapiServiceProvider } from '../SwapiServiceContext';
 import ErrorBoundry from '../ErrorBoundry';
 
 import { PersonList, PersonDetails, PlanetList, PlanetDetails, StarshipList, StarshipDetails } from '../StarWarsComponents';
@@ -35,30 +31,22 @@ export default class App extends Component {
 
     return (
         <ErrorBoundry>
-          <Header />
-          <RandomPlanet />
+          <SwapiServiceProvider value={this.swapiService}>
+            <Header />
+            <RandomPlanet />
 
-          <PersonDetails itemId={11}/>
+            <PersonDetails itemId={11}/>
 
-          <PlanetDetails itemId={11}/>
+            <PlanetDetails itemId={11}/>
 
-          <StarshipDetails itemId={11}/>
+            <StarshipDetails itemId={11}/>
 
-          <PersonList />
+            <PersonList />
 
-          <StarshipList />
+            <StarshipList />
 
-          <PlanetList />
-          {/* <PeoplePage />
-          
-          <ItemDetails 
-            getData={this.swapiService.getStarship}
-            itemId={9} 
-            getImageUrl={this.swapiService.getStarshipImage}>
-            <Record field="model" label="Model" />
-            <Record field="length" label="Length" />
-            <Record field="costInCredits" label="cost" />
-          </ItemDetails> */}
+            <PlanetList />
+          </SwapiServiceProvider>
         </ErrorBoundry>
     );
   }
--- react-apps/star-db/src/components/StarWarsComponents/Details.jsx
+++ react-apps/star-db/src/components/StarWarsComponents/Details.jsx
@@ -1,48 +1,68 @@
 import React from 'react';
 import ItemDetails from '../ItemDetails';
-import SwapiService from '../../services/swapi-service';
+import { SwapiServiceConsumer } from '../SwapiServiceContext';
 import Record from '../Record';
-
-const swapiService = new SwapiService();
-
-const { getPerson, getStarship, getPlanet, getPersonImage, getStarshipImage, getPlanetImage } = swapiService;
 
 const PersonDetails = ({itemId}) => {
   return  (
-    <ItemDetails 
-          itemId={itemId}
-          getData={getPerson}
-          getImageUrl={getPersonImage}>
-        <Record field="gender" label="Gender"/>
-        <Record field="birthYear" label="Birth Year"/>
-        <Record field="eyeColor" label="Eye Color"/>
-      </ItemDetails>
+    <SwapiServiceConsumer>
+      {
+        ({getPerson, getPersonImage}) => {
+          return (
+            <ItemDetails 
+                itemId={itemId}
+                getData={getPerson}
+                getImageUrl={getPersonImage}>
+              <Record field="gender" label="Gender"/>
+              <Record field="birthYear" label="Birth Year"/>
+              <Record field="eyeColor" label="Eye Color"/>
+            </ItemDetails>
+          );
+        }
+      }
+    </SwapiServiceConsumer>
   );
 };
 
 const StarshipDetails = ({itemId}) => {
   return (
-    <ItemDetails 
-          itemId={itemId}
-          getData={getStarship}
-          getImageUrl={getStarshipImage}>
-        <Record field="model" label="Model" />
-        <Record field="length" label="Length" />
-        <Record field="costInCredits" label="cost" />
-      </ItemDetails>
+    <SwapiServiceConsumer>
+      {
+        ({getStarship, getStarshipImage}) => {
+          return (
+            <ItemDetails 
+                itemId={itemId}
+                getData={getStarship}
+                getImageUrl={getStarshipImage}>
+              <Record field="model" label="Model" />
+              <Record field="length" label="Length" />
+              <Record field="costInCredits" label="cost" />
+            </ItemDetails>
+          );
+        }
+      }
+    </SwapiServiceConsumer>
   );
 };
 
 const PlanetDetails = ({itemId}) => {
   return (
-    <ItemDetails 
-          itemId={itemId}
-          getData={getPlanet}
-          getImageUrl={getPlanetImage}>
-        <Record field="population" label="Population" />
-        <Record field="rotationPeriod" label="Rotation Period" />
-        <Record field="diameter" label="Diameter" />
-      </ItemDetails>
+    <SwapiServiceConsumer>
+      {
+        ({getPlanet, getPlanetImage}) => {
+          return (
+            <ItemDetails 
+                itemId={itemId}
+                getData={getPlanet}
+                getImageUrl={getPlanetImage}>
+              <Record field="population" label="Population" />
+              <Record field="rotationPeriod" label="Rotation Period" />
+              <Record field="diameter" label="Diameter" />
+            </ItemDetails>
+          );
+        }
+      }
+    </SwapiServiceConsumer>
   );
 };
 
