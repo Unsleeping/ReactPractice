c82963450b9a73b2448ec363d0589d4a944eaf2f
Unsleeping <mandrov.ad17@physics.msu.ru>
2020-09-30 18:05:55 +0300

    add routing

--- react-apps/star-db/src/components/App/App.jsx
+++ react-apps/star-db/src/components/App/App.jsx
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.scss';
 
 import Header from '../Header';
 import RandomPlanet from '../RandomPlanet';
 import ErrorIndicator from '../ErrorIndicator';
 import SwapiService from '../../services/swapi-service';
-import { SwapiServiceProvider } from '../SwapiServiceContext';
 import ErrorBoundry from '../ErrorBoundry';
 import PeoplePage from '../Pages/PeoplePage';
 import PlanetPage from '../Pages/PlanetsPage';
 import StarshipsPage from '../Pages/StarshipsPage';
+import { SwapiServiceProvider } from '../SwapiServiceContext';
 
 export default class App extends Component {
 
@@ -30,17 +31,17 @@ export default class App extends Component {
     }
 
     return (
-        <ErrorBoundry>
-          <SwapiServiceProvider value={this.swapiService}>
+      <ErrorBoundry>
+        <SwapiServiceProvider value={this.swapiService}>
+          <Router>
             <Header />
             <RandomPlanet />
-
-            <PeoplePage />
-            <PlanetPage />
-            <StarshipsPage />
-            
-          </SwapiServiceProvider>
-        </ErrorBoundry>
+            <Route path="/people" component={PeoplePage} />
+            <Route path="/planets" component={PlanetPage} />
+            <Route path="/starships" component={StarshipsPage} />
+          </Router> 
+        </SwapiServiceProvider>
+      </ErrorBoundry>
     );
   }
 }
