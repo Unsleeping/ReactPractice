cfca4c45ca74d35582864ca0bcc6e89013d6c115
Unsleeping <mandrov.ad17@physics.msu.ru>
2020-10-04 13:21:35 +0300

    action creators

--- react-apps/redux-sandbox/src/index.js
+++ react-apps/redux-sandbox/src/index.js
@@ -5,6 +5,9 @@ const reducer = (state = 0, action) => {
   switch (action.type) {
     case 'INC':
       return state + 1;
+    
+    case 'RND':
+      return state + action.payload;
 
     case 'DEC':
       return state - 1;
@@ -16,16 +19,27 @@ const reducer = (state = 0, action) => {
 
 const store = createStore(reducer);
 
+const inc = () => ({ type: 'INC' });
+const dec = () => ({ type: 'DEC' });
+const rnd = (payload) => ({ type: 'RND', payload });
+
 document
   .getElementById('inc')
   .addEventListener('click', () => {
-    store.dispatch({type: 'INC'});
+    store.dispatch(inc());
   });
 
 document
   .getElementById('dec')
   .addEventListener('click', () => {
-    store.dispatch({type: 'DEC'});
+    store.dispatch(dec());
+  });
+
+document
+  .getElementById('rnd')
+  .addEventListener('click', () => {
+    const payload = Math.floor(Math.random() * 10 + 1);
+    store.dispatch(rnd(payload));
   });
 
 const update = () => {
