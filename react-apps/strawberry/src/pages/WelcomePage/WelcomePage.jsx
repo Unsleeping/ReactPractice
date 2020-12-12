9c44d2e1b37c3e1bda463daf74b90ddf5d78861e
Unsleeping <mandrov.ad17@physics.msu.ru>
2020-12-12 03:16:31 +0300

    primary welcome page

--- react-apps/strawberry/src/pages/WelcomePage/WelcomePage.jsx
+++ react-apps/strawberry/src/pages/WelcomePage/WelcomePage.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import Footer from '../../components/Footer';
+import Header from '../../components/Header';
 import './WelcomePage.scss';
 
 const WelcomePage = () => {
   return (
     <>
       <div className="WelcomePage">
+        <Header />
         <div className="WelcomePage-wrapper">
-          <h1>Welcome to our website</h1>
+          <h1>Добро пожаловать на наш сайт</h1>
           <Link to="/signup" className="reset-link">
             <Button type="button" variant="outlined" color="primary">
-              Get started
+              Начать использовать
             </Button>
           </Link>
         </div>
