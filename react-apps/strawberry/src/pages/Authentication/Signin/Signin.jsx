f195a7cf41f5b92698458d556c19023b2e892c4f
Unsleeping <mandrov.ad17@physics.msu.ru>
2020-12-12 03:18:04 +0300

    add auth logic with validation

--- react-apps/strawberry/src/pages/Authentication/Signin/Signin.jsx
+++ react-apps/strawberry/src/pages/Authentication/Signin/Signin.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -11,11 +12,19 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import { signin } from '../../../services/authentication';
+import {
+  checkUserEmail,
+  checkUserPassword,
+  getTokenFromLocalStorage,
+  saveTokenToLocalStorage,
+} from '../../../utils/utils';
+import { setAuthenticationState } from '../../../redux/ducks/authentication';
 import './Signin.scss';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
-    marginTop: theme.spacing(8),
+    paddingTop: theme.spacing(8),
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
@@ -33,62 +42,150 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function Signin() {
+const Signin = ({ history }) => {
+  const dispatch = useDispatch();
   const classes = useStyles();
 
+  const [isSendingRequest, setIsSendingRequest] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [isErrorEmail, setIsErrorEmail] = useState(false);
+  const [isErrorPassword, setIsErrorPassword] = useState(false);
+
+  useEffect(() => {
+    setIsSendingRequest(false);
+  }, [errorMessage]);
+
+  const onChangeHandler = (event, setter, validator, errorSetter) => {
+    setErrorMessage('');
+    if (validator(event.target.value)) errorSetter(false);
+    setter(event.target.value);
+  };
+
+  const validateInputs = () => {
+    if (!checkUserEmail(email)) setIsErrorEmail(true);
+    if (!password) setIsErrorPassword(true);
+  };
+
+  const onSigninClick = async () => {
+    validateInputs();
+    if (checkUserEmail(email) && password && !errorMessage) {
+      if (isSendingRequest === false) {
+        setIsSendingRequest(true);
+        const response = await signin({
+          email: email,
+          password: password,
+        });
+        if (response.token) {
+          history.push('/');
+          saveTokenToLocalStorage(response.token);
+          getTokenFromLocalStorage();
+          dispatch(setAuthenticationState(true));
+        } else {
+          setErrorMessage('Пользователь не существует');
+        }
+        if (!response) setErrorMessage('Проблемы с сервером, попробуйте позже');
+      }
+    }
+  };
+
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          {/* <FormControlLabel
+    <div className="Signin">
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Авторизация
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              error={
+                isErrorEmail ||
+                (!!errorMessage &&
+                  errorMessage !== 'Проблемы с сервером, попробуйте позже')
+              }
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Электронный адрес"
+              name="email"
+              autoComplete="email"
+              value={email}
+              onChange={(event) =>
+                onChangeHandler(
+                  event,
+                  setEmail,
+                  checkUserEmail,
+                  setIsErrorEmail
+                )
+              }
+              helperText={
+                isErrorEmail &&
+                (email
+                  ? 'Проверьте правильность ввода электронного адреса'
+                  : 'Электронный адрес обязателен')
+              }
+              autoFocus
+            />
+            <TextField
+              error={
+                isErrorPassword ||
+                (!!errorMessage &&
+                  errorMessage !== 'Проблемы с сервером, попробуйте позже')
+              }
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Пароль"
+              value={password}
+              onChange={(event) =>
+                onChangeHandler(
+                  event,
+                  setPassword,
+                  checkUserPassword,
+                  setIsErrorPassword
+                )
+              }
+              helperText={
+                (isErrorPassword && 'Пароль обязателен') || errorMessage
+              }
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            {/* <FormControlLabel
             control={<Checkbox value="remember" color="primary" />}
             label="Remember me"
           /> */}
-          <Button
-            type="button"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign In
-          </Button>
-          <Grid container justify="flex-end">
-            <Grid item>
-              <Link to="/signup">Don't have an account? Sign Up</Link>
+            <Button
+              disabled={!!errorMessage || isErrorEmail || isErrorPassword}
+              type="button"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+              onClick={onSigninClick}
+            >
+              {isSendingRequest ? '...' : 'Войти'}
+            </Button>
+            <Grid container justify="flex-end">
+              <Grid item>
+                <Link to="/signup">Нет аккаунта? Зарегистрироваться</Link>
+              </Grid>
             </Grid>
-          </Grid>
-        </form>
-      </div>
-    </Container>
+          </form>
+        </div>
+      </Container>
+    </div>
   );
-}
+};
+
+export default withRouter(Signin);
--- react-apps/strawberry/src/pages/Authentication/Signup/Signup.jsx
+++ react-apps/strawberry/src/pages/Authentication/Signup/Signup.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -10,12 +11,21 @@ import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import { signup } from '../../../services/authentication';
+import {
+  checkUserEmail,
+  checkUserPassword,
+  getTokenFromLocalStorage,
+  saveTokenToLocalStorage,
+  emptyChecker,
+} from '../../../utils/utils';
+import { setAuthenticationState } from '../../../redux/ducks/authentication';
 import Container from '@material-ui/core/Container';
 import './Signup.scss';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
-    marginTop: theme.spacing(8),
+    paddingTop: theme.spacing(8),
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
@@ -33,90 +43,214 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function SignUp() {
+const SignUp = ({ history }) => {
+  const dispatch = useDispatch();
   const classes = useStyles();
 
+  const [isSendingRequest, setIsSendingRequest] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [firstname, setFirstname] = useState('');
+  const [lastname, setLastname] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [isErrorEmail, setIsErrorEmail] = useState(false);
+  const [isErrorPassword, setIsErrorPassword] = useState(false);
+  const [isErrorFirstname, setIsErrorFirstname] = useState(false);
+  const [isErrorLastname, setIsErrorLastname] = useState(false);
+
+  useEffect(() => {
+    setIsSendingRequest(false);
+  }, [errorMessage]);
+
+  const onChangeHandler = (event, setter, validator, errorSetter) => {
+    setErrorMessage('');
+    if (validator(event.target.value)) errorSetter(false);
+    setter(event.target.value);
+  };
+
+  const validateInputs = () => {
+    if (!checkUserEmail(email)) setIsErrorEmail(true);
+    if (!checkUserPassword(password)) setIsErrorPassword(true);
+    if (!firstname) setIsErrorFirstname(true);
+    if (!lastname) setIsErrorLastname(true);
+  };
+
+  const onSignupClick = async () => {
+    validateInputs();
+    if (checkUserEmail(email) && checkUserPassword(password) && !errorMessage) {
+      if (isSendingRequest === false) {
+        setIsSendingRequest(true);
+        const response = await signup({
+          email: email,
+          password: password,
+          profile: {
+            first_name: firstname,
+            last_name: lastname,
+          },
+        });
+        if (response.token) {
+          saveTokenToLocalStorage(response.token);
+          getTokenFromLocalStorage();
+          dispatch(setAuthenticationState(true));
+          history.push('/');
+        }
+        if (!response.success)
+          setErrorMessage('Проблемы с сервером, попробуйте позже');
+      }
+    }
+  };
+
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign up
-        </Typography>
-        <form className={classes.form} noValidate>
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                autoComplete="fname"
-                name="firstName"
-                variant="outlined"
-                required
-                fullWidth
-                id="firstName"
-                label="First Name"
-                autoFocus
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="lastName"
-                label="Last Name"
-                name="lastName"
-                autoComplete="lname"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-            </Grid>
-            {/* <Grid item xs={12}>
+    <div className="Signup">
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Регистрация
+          </Typography>
+          <form className={classes.form} noValidate>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="fname"
+                  name="firstName"
+                  variant="outlined"
+                  required
+                  fullWidth
+                  value={firstname}
+                  error={isErrorFirstname}
+                  onChange={(event) =>
+                    onChangeHandler(
+                      event,
+                      setFirstname,
+                      emptyChecker,
+                      setIsErrorFirstname
+                    )
+                  }
+                  helperText={isErrorFirstname && 'Имя обязательно'}
+                  id="firstName"
+                  label="Имя"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Фамилия"
+                  name="lastName"
+                  autoComplete="lname"
+                  value={lastname}
+                  error={isErrorLastname}
+                  onChange={(event) =>
+                    onChangeHandler(
+                      event,
+                      setLastname,
+                      emptyChecker,
+                      setIsErrorLastname
+                    )
+                  }
+                  helperText={isErrorLastname && 'Фамилия обязательна'}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  error={isErrorEmail}
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Электронный адрес"
+                  name="email"
+                  autoComplete="email"
+                  value={email}
+                  onChange={(event) =>
+                    onChangeHandler(
+                      event,
+                      setEmail,
+                      checkUserEmail,
+                      setIsErrorEmail
+                    )
+                  }
+                  helperText={
+                    isErrorEmail &&
+                    (email
+                      ? 'Проверьте правильность ввода электронного адреса'
+                      : 'Электронный адрес обязателен')
+                  }
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  error={isErrorPassword}
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Пароль"
+                  value={password}
+                  onChange={(event) =>
+                    onChangeHandler(
+                      event,
+                      setPassword,
+                      checkUserPassword,
+                      setIsErrorPassword
+                    )
+                  }
+                  helperText={
+                    (isErrorPassword &&
+                      (password
+                        ? 'Пароль должен содержать не менее 8-ми символов, в том числе цифры, прописные и строчные буквы'
+                        : 'Пароль обязателен')) ||
+                    errorMessage
+                  }
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+              </Grid>
+              {/* <Grid item xs={12}>
               <FormControlLabel
                 control={<Checkbox value="allowExtraEmails" color="primary" />}
                 label="I want to receive inspiration, marketing promotions and updates via email."
               />
             </Grid> */}
-          </Grid>
-          <Button
-            type="button"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign Up
-          </Button>
-          <Grid container justify="flex-end">
-            <Grid item>
-              <Link to="/signin">Already have an account? Sign in</Link>
             </Grid>
-          </Grid>
-        </form>
-      </div>
-    </Container>
+            <Button
+              disabled={
+                !!errorMessage ||
+                isErrorPassword ||
+                isErrorEmail ||
+                isErrorFirstname ||
+                isErrorLastname
+              }
+              type="button"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+              onClick={onSignupClick}
+            >
+              {isSendingRequest ? '...' : 'Зарегистрироваться'}
+            </Button>
+            <Grid container justify="flex-end">
+              <Grid item>
+                <Link to="/signin">Уже есть аккаунт? Войти</Link>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+      </Container>
+    </div>
   );
-}
+};
+
+export default withRouter(SignUp);
