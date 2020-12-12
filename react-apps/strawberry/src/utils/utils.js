bfaafeb61273f7b7c798c9931315797641e150cf
Unsleeping <mandrov.ad17@physics.msu.ru>
2020-12-12 03:18:41 +0300

    update utils with validate  handlers

--- react-apps/strawberry/src/utils/utils.js
+++ react-apps/strawberry/src/utils/utils.js
@@ -5,3 +5,21 @@ export const saveTokenToLocalStorage = (token) => {
 export const getTokenFromLocalStorage = () => {
   window.token = localStorage.token;
 };
+
+export const checkUserEmail = (email) => {
+  if (
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      email
+    )
+  )
+    return true;
+  return false;
+};
+
+export const checkUserPassword = (password) => {
+  if (/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm.test(password))
+    return true;
+  return false;
+};
+
+export const emptyChecker = (value) => !!value;
