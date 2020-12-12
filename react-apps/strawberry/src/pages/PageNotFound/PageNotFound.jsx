91b2541639439acb8303750c546ad0b04b23d327
Unsleeping <mandrov.ad17@physics.msu.ru>
2020-12-12 16:42:26 +0300

    fix classname

--- react-apps/strawberry/src/pages/PageNotFound/PageNotFound.jsx
+++ react-apps/strawberry/src/pages/PageNotFound/PageNotFound.jsx
@@ -15,15 +15,15 @@ const PageNotFound = () => {
           </Button>
         </Link>
       </div>
-      <div class="waveWrapper waveAnimation">
-        <div class="waveWrapperInner bgTop">
-          <div class="wave waveTop"></div>
+      <div className="waveWrapper waveAnimation">
+        <div className="waveWrapperInner bgTop">
+          <div className="wave waveTop"></div>
         </div>
-        <div class="waveWrapperInner bgMiddle">
-          <div class="wave waveMiddle"></div>
+        <div className="waveWrapperInner bgMiddle">
+          <div className="wave waveMiddle"></div>
         </div>
-        <div class="waveWrapperInner bgBottom">
-          <div class="wave waveBottom"></div>
+        <div className="waveWrapperInner bgBottom">
+          <div className="wave waveBottom"></div>
         </div>
       </div>
     </div>
